b55967ba5f50d5d89cfc2a68708de32dd0ba2b2e
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-26 17:15:15 +0200

    Update signal.py

--- backend/models/signal.py
+++ backend/models/signal.py
@@ -1,1 +1,21 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Signal:
+
+    time: str
+
+    market: str
+
+    asset: str
+
+    action: str
+
+    size: str
+
+    who: str
+
+    source: str
+
+    score: int
