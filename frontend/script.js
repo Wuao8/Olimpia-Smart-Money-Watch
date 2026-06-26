b0bf3c61d909724f405e5f4f58cff4c0cd01e014
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-26 16:52:50 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,41 @@
+const API="http://127.0.0.1:8000/api/live";
 
+async function loadData(){
+
+const response=await fetch(API);
+
+const data=await response.json();
+
+const table=document.getElementById("tableBody");
+
+table.innerHTML="";
+
+data.signals.forEach(signal=>{
+
+table.innerHTML+=`
+
+<tr>
+
+<td>${signal.market}</td>
+
+<td>${signal.asset}</td>
+
+<td>${signal.action}</td>
+
+<td>${signal.size}</td>
+
+<td>${signal.who}</td>
+
+<td>${signal.source}</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+loadData();
+
+setInterval(loadData,10000);
