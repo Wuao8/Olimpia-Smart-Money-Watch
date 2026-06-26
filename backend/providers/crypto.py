e0c2d25eed74161534c8c7b17a418ea8a49787d4
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-26 17:15:44 +0200

    Update crypto.py

--- backend/providers/crypto.py
+++ backend/providers/crypto.py
@@ -1,1 +1,32 @@
+from models.signal import Signal
 
+
+def get_crypto_signals():
+
+    signals = []
+
+    signals.append(
+
+        Signal(
+
+            time="LIVE",
+
+            market="Crypto",
+
+            asset="BTC",
+
+            action="BUY",
+
+            size="25 M$",
+
+            who="Wallet -----",
+
+            source="Demo",
+
+            score=94
+
+        )
+
+    )
+
+    return signals
