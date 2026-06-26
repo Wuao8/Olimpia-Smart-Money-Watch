2d6941d379ccc7e250aaaf73f9ccbe6ee611da55
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-26 17:16:09 +0200

    Update stocks.py

--- backend/providers/stocks.py
+++ backend/providers/stocks.py
@@ -1,1 +1,32 @@
+from models.signal import Signal
 
+
+def get_stock_signals():
+
+    signals = []
+
+    signals.append(
+
+        Signal(
+
+            time="LIVE",
+
+            market="Stocks",
+
+            asset="AAPL",
+
+            action="BUY",
+
+            size="800 K$",
+
+            who="CEO",
+
+            source="Demo",
+
+            score=82
+
+        )
+
+    )
+
+    return signals
