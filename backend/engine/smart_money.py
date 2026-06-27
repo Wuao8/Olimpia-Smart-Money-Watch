dddaa1456a1c0c7ce00f58781643606d3359347b
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-27 12:28:02 +0200

    Update smart_money.py

--- backend/engine/smart_money.py
+++ backend/engine/smart_money.py
@@ -1,17 +1,11 @@
 from providers.crypto.coingecko import get_crypto_signals
-from providers.stocks.finnhub import get_stock_signals
 
 
 def get_all_signals():
-
     signals = []
 
     signals.extend(get_crypto_signals())
-    signals.extend(get_stock_signals())
 
-    signals.sort(
-        key=lambda signal: signal.score,
-        reverse=True
-    )
+    signals.sort(key=lambda s: s.score, reverse=True)
 
     return signals
